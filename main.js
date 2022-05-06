40143af685d4b85fbac9317fabe6e79878c592a3
Bogdan Serdyuk <bogdanserd.bs@gmail.com>
2022-05-06 22:32:46 +0300

    Added checkbox

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 window.onload = function () {
 
-    const btn = document.querySelector('.btn')
+
     const input = document.querySelector('.input')
     const list = document.querySelector('.list')
+
     let arrOfList = [];
+    if (localStorage.getItem('todo') != undefined) {
+        arrOfList = JSON.parse(localStorage.getItem('todo'));
+    }
 
-    btn.onclick = () => {
-
+    document.querySelector('.btn').onclick = function () {
+        // arrOfList = []
         let elements = {};
         elements.todo = input.value;
         elements.check = false;
         arrOfList.push(elements);
 
-
         out()
+
+        localStorage.setItem('todo', JSON.stringify(arrOfList))
     }
 
     function out() {
         let out = '';
+
         if (input.value !== '') {
             let listItem = document.createElement('li');
             listItem.classList.add('li');
             list.appendChild(listItem)
             input.value = '';
+
+
 
             for (let key in arrOfList) {
                 out = arrOfList[key].todo;
@@ -30,7 +38,10 @@ window.onload = function () {
 
             listItem.textContent = out;
             deleteItem(listItem);
+            checked(listItem)
         }
+
+
     }
 
     function deleteItem(item) {
@@ -42,6 +53,24 @@ window.onload = function () {
         deleteBtn.addEventListener('click', () => {
             list.removeChild(item)
         })
+    }
+
+    function checked(item) {
+        let inputCheck = document.createElement("input");
+        inputCheck.type = "checkbox";
+        item.appendChild(inputCheck)
+
+        let allItems = document.querySelectorAll('li');
+        console.log(allItems)
+        for (let key of allItems) {
+
+            if (inputCheck.checked) {
+                key.classList.add('checked')
+            } else {
+                key.classList.remove('checked')
+            }
+        }
+
     }
 
 
