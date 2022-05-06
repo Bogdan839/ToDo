fe9561b75a2ce480d8b04a4688c562c63d2a8f57
Bogdan Serdyuk <bogdanserd.bs@gmail.com>
2022-05-06 17:46:25 +0300

    added delete btn

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const input = document.querySelector('.input')
 const btn = document.querySelector('.btn')
 const list = document.querySelector('.list')
-const deleteBtn = document.querySelector('.min-btn')
+// const deleteBtn = document.querySelector('.min-btn')
 
 btn.addEventListener('click', () => {
     if (input.value !== '') {
-        createList(input.value)
+        createElement(input.value)
     }
     input.value = '';
 })
 
-function createList(value) {
+function createElement(value) {
     const listItem = document.createElement('li')
     listItem.classList.add('li')
     list.appendChild(listItem)
     listItem.textContent = value;
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('min-btn')
+    listItem.appendChild(deleteBtn)
+    deleteBtn.textContent = 'D';
+
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(listItem)
+    })
 }
